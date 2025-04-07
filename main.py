2b47a34ae4abc9aba97d74de65d21df4a8fad426
vjovkovs <vladimir.jovkovski@gmail.com>
2025-04-07 14:23:55 -0400

    completed project

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
+import random
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
-
+from player import Player
+from shot import Shot
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-   # Initialize pygame
     pygame.init()
-    
-    # Create a window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    # Game loop
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        # Check for player inputs (event handling)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        # Update the game world
-        # (This will be implemented in future steps)
-        
-        # Draw the game to the screen
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+   
 
 
 if __name__ == "__main__":
---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+        # self.color = SHOT_COLOR
+        # self.speed = SHOT_SPEED
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
